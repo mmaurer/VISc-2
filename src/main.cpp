19c5bb9deda5b25430e81a2c124d22a1940c118b
Mauricio Maurer <mauricio.maurer@gmail.com>
2012-03-04 15:02:55 -0500

    Adding copyright notice to all source files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-/// UNDONE: application icon Mc OS/X
-/// UNDONE: application icon Linux
-/// UNDONE: add tools to enable/disable volumes (in this case, more than one volume can be loaded)
-/// UNDONE: correct background color in saved screenshots
+/*
+  VISc 2 is a simple volume renderer.
 
+  Copyright 2012 Mauricio Maurer (mauricio.maurer@gmail.com)
+
+  This file is part of VISc 2.
+
+  VISc 2 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  VISc 2 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with VISc 2.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// TODO: correct background color in saved screenshots
 /// TODO: Eliminate memory leaks
 /// TODO: Use Qt's shaders instead of glut or whatever
 /// TODO: Clean up and bring the code up to a finer standard
